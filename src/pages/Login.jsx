ba8c092ff191eaec58c3d0f7f672846b122ffd05
sanjoy <sanjoylab66@gmail.com>
2024-12-20 22:58:49 +0600

    Get Login User Info From firestore

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,27 +1,34 @@
 import { signInWithEmailAndPassword } from "firebase/auth";
-import { auth } from "../config/firebaseConfig";
+import { auth, db } from "../config/firebaseConfig";
+import { getDoc, doc } from "firebase/firestore";
 
 export default function Login() {
+  const handleSubmit = async (e) => {
+    e.preventDefault();
 
-    const handleSubmit = async (e) => {
-      e.preventDefault();
-  
-      let email = document.getElementById("userEmail").value;
-      let password = document.getElementById("userPassword").value;
-      
-      try {
-        await signInWithEmailAndPassword(auth, email,password);
+    let email = document.getElementById("userEmail").value;
+    let password = document.getElementById("userPassword").value;
 
-        auth.onAuthStateChanged(async(user)=>{
-          console.log(user);
-        })
+    try {
+      await signInWithEmailAndPassword(auth, email, password);
 
-        console.log("Login successfully");
+      auth.onAuthStateChanged(async (user) => {
+        console.log(user);
 
-      } catch (error) {
-        console.log(error.message);
-      }
-    };
+        const docRef = doc(db, "users", user.uid);
+        const docSnap = await getDoc(docRef);
+        if (docSnap.exists()) {
+          console.log(docSnap.data());
+        } else {
+          console.log("User is not login");
+        }
+      });
+
+      console.log("Login successfully");
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
 
   return (
     <div>
