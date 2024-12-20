d049ae14672250977d7b7ea56d5e6412e7b87f8e
sanjoy <sanjoylab66@gmail.com>
2024-12-20 22:24:41 +0600

    Register User Data Save firebase/firestore

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,28 +1,43 @@
 import { createUserWithEmailAndPassword } from "firebase/auth";
-import { auth } from "../config/firebaseConfig";
+import { auth, db } from "../config/firebaseConfig";
+import { setDoc, doc } from "firebase/firestore";
 
 export default function Register() {
-
-  const handleSubmit = async(e)=>{
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    let email = document.getElementById('email').value;
-    let password = document.getElementById('password').value;
+
+    let userName = document.getElementById("userName").value;
+    let email = document.getElementById("email").value;
+    let password = document.getElementById("password").value;
     try {
-      await createUserWithEmailAndPassword(auth,email,password);
+      await createUserWithEmailAndPassword(auth, email, password);
       const user = auth.currentUser;
+
+      if (user) {
+        await setDoc(doc(db, "users", user.uid), {
+          userName,
+          email: user.email
+        });
+      }
+
       console.log(user);
       console.log("Register successfully");
     } catch (error) {
-      console.log(error);
+      console.log(error.message);
     }
-  }
+  };
 
   return (
     <div>
       <form onSubmit={handleSubmit}>
+        <label htmlFor="userName">User Name : </label>
+        <input type="text" name="" id="userName" />
+        <br />
+        <label htmlFor="email">Email : </label>
         <input type="email" name="" id="email" />
         <br />
-        <input type="text" name="" id="password" />
+        <label htmlFor="password">Password : </label>
+        <input type="password" name="" id="password" />
         <br />
         <input type="submit" value="Register" />
       </form>
